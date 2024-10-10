01953f3589ff25102b4afdcbb13e072b19b5147d
unknown <johnadibe123@gmail.com>
2024-10-10 22:15:16 +0100

    Implement tanstack table setup

---
+++ app/dashboard/products/columns.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal, MoreHorizontalIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Image from "next/image"
+
+type ProductColumn = {
+    id: number
+    title: string
+    price: number
+    image: string
+    variants: any
+}
+
+export const columns: ColumnDef<ProductColumn>[] = [
+    {
+        accessorKey: "id",
+        header: "ID",
+    },
+    {
+        accessorKey: "title",
+        header: "Title",
+    },
+    {
+        accessorKey: "variants",
+        header: "Variants",
+    },
+    {
+        accessorKey: "price",
+        header: "Price",
+        cell: ({row}) => {
+            const price = parseFloat(row.getValue("price"))
+            const formatted = new Intl.NumberFormat("en-US", {
+                currency: "NGN",
+                style: "currency"
+            }).format(price)
+            return (
+            <div className="font-medium text-xs">
+                {formatted}
+            </div>
+            )
+        }
+    },
+    {
+        accessorKey: "image",
+        header: "Image",
+        cell: ({ row }) => {
+            const cellImage = row.getValue("image") as string
+            const cellTitle = row.getValue("title") as string
+            return (
+                <div>
+                    <Image src={cellImage} alt={cellTitle} width={50} height={50} className="rounded-md" />
+                </div>
+            )
+        }
+    },
+    {
+        accessorKey: "actions",
+        header: "Actions",
+        cell: ({ row }) => {
+            const product = row.original
+            return (
+                <DropdownMenu modal={false}>
+  <DropdownMenuTrigger asChild>
+    <Button variant={"ghost"} className="h-8 w-8 p-0"><MoreHorizontalIcon className="h-4 w-4" /></Button>
+    </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem className="dark:focus:bg-primary focus:bg-primary/50 cursor-pointer">Edit Product</DropdownMenuItem>
+    <DropdownMenuItem className="dark:focus:bg-destructive focus:bg-destructive/50 cursor-pointer">Delete Product</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+            )
+        }
+    },
+]
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,28 @@
-export default function Products() {
+import { db } from "@/server"
+import placeholder from "@/public/placeholder-image.jpg"
+import { DataTable } from "./data-table"
+import { columns } from "./columns"
+
+export default async function Products() {
+    // fetch the products from the database
+    const products = await db.query.products.findMany({
+        orderBy: (product, { desc }) => [desc(product.id)]
+    })
+    if(!products) throw new Error("No products found")
+
+    const dataTable = products.map((product) => {
+        return {
+            id: product.id,
+            title: product.title,
+            price: product.price,
+            variants: [],
+            image: placeholder.src,
+        }
+    })
+    if(!dataTable) throw new Error("No data found")
     return (
         <div>
-            <h1>Products</h1>
+            <DataTable columns={columns} data={dataTable} />
         </div>
     )
 }
