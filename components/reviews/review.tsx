0d048a266685857b34d1b987f6f66f1fc33603be
unknown <johnadibe123@gmail.com>
2024-10-25 12:44:25 +0100

    Implement star rating system

--- components/reviews/review.tsx
+++ components/reviews/review.tsx
@@ -1,2 +1,34 @@
 // This component is the card of the one single review
 "use client"
+
+import { ReviewsWithUser } from "@/lib/infer-type"
+import { motion } from "framer-motion"
+import { Card } from "../ui/card"
+import Image from "next/image"
+import { formatDistance, subDays } from 'date-fns'
+import Stars from "./stars"
+
+export default function Review({ reviews }: { reviews: ReviewsWithUser[] }) {
+    return (
+        <motion.div className="flex flex-col gap-4">
+            {reviews.map((review) => (
+                <Card key={review.id} className="p-4">
+                    <div className="flex gap-2 items-center">
+                        <Image className="rounded-full" src={review.user?.image!} width={32} height={32} alt={review.user.name!} />
+                        <div>
+                            <p className="text-sm font-bold">{review.user.name}</p>
+                            <div className="flex items-center gap-2">
+                                {/* star componanet */}
+                                <Stars rating={review.rating} />
+                                {/* display when the review was added. we will use date-fns */}
+                                <p className="text-xs text-bold text-muted-foreground">{formatDistance(subDays(review.created!, 0), new Date())}</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* reviews comment */}
+                    <p className="py-2 font-medium">{review.comment}</p>
+                </Card>
+            ))}
+        </motion.div>
+    )
+}
--- components/reviews/reviews.tsx
+++ components/reviews/reviews.tsx
@@ -1,13 +1,29 @@
 // This is where we are going to put the reviews form component and everything
 
+import { db } from "@/server";
+import Review from "./review";
 import ReviewsForm from "./reviews-form";
+import { desc, eq } from "drizzle-orm";
+import { reviews } from "@/server/schema";
 
 export default async function Reviews({ productID }: { productID: number }) {
+    const data = await db.query.reviews.findMany({
+        // we need to get the user too
+        with: { user: true },
+        // we need to find the reviews that are matching the actually product
+        where: eq(reviews.productID, productID),
+        orderBy: [desc(reviews.created)]
+    })
     return (
         <section className="py-8">
             <h2 className="text-2xl font-bold mb-4">Product Reviews</h2>
-            <div>
-                <ReviewsForm />
+            <div className="flex gap-2 lg:gap-12 justify-stretch lg:flex-row flex-col">
+                <div className="flex-1">
+                    <Review reviews={data} />
+                </div>
+                <div className="flex-1 flex flex-col gap-2">
+                    <ReviewsForm />
+                </div>
             </div>
         </section>
     )
